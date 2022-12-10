d9b07eec948d11de967ca41df48919876122412f
Nykolai Topchyi <nodetempest@gmail.com>
2022-12-10 20:42:21 +0200

    remove bloat

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Styles } from "./Styles";
-import App from "./App";
+import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
