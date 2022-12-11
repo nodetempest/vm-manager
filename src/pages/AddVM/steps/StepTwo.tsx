d0bfe0f01be6ea3363f17a62ce5ee173c6667e33
Nykolai Topchyi <nodetempest@gmail.com>
2022-12-11 17:52:00 +0200

    style TreeView

--- src/pages/AddVM/steps/StepTwo.tsx
+++ src/pages/AddVM/steps/StepTwo.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Checkbox } from "@mui/material";
 import { TreeView, TreeItem } from "@mui/lab";
 import { ExpandMore, ChevronRight } from "@mui/icons-material";
 
@@ -12,26 +13,63 @@ type TCluster = {
 
 const clusters: TCluster[] = untypedClusters;
 
-export class StepTwo extends React.Component {
+export type TStepTwoState = {
+  checkedId: string;
+};
+export class StepTwo extends React.Component<{}, TStepTwoState> {
+  state = {
+    checkedId: "",
+  };
+
+  renderCluster = (cluster: TCluster[]) => {
+    return cluster.map((cluster) => (
+      <TreeItem
+        nodeId={cluster.value}
+        key={cluster.value}
+        label={
+          cluster?.children ? (
+            cluster.label
+          ) : (
+            <>
+              <Checkbox
+                sx={{ p: "2px" }}
+                size="small"
+                onChange={() =>
+                  this.setState((prev) => ({
+                    checkedId:
+                      prev.checkedId === cluster.value ? "" : cluster.value,
+                  }))
+                }
+                checked={this.state.checkedId === cluster.value}
+              />{" "}
+              {cluster.label}
+            </>
+          )
+        }
+        sx={{
+          "& .MuiTreeItem-content.Mui-focused": {
+            bgcolor: "transparent",
+          },
+          "& .MuiTreeItem-content:hover": {
+            bgcolor: "transparent",
+          },
+        }}
+      >
+        {cluster?.children && this.renderCluster(cluster.children)}
+      </TreeItem>
+    ));
+  };
+
   render() {
     return (
       <TreeView
-        aria-label="file system navigator"
         defaultCollapseIcon={<ExpandMore />}
         defaultExpandIcon={<ChevronRight />}
         sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
+        disableSelection
+        defaultExpanded={clusters.map((cluster) => cluster.value)}
       >
-        {(function renderCluster(cluster) {
-          return cluster.map((cluster) => (
-            <TreeItem
-              label={cluster.label}
-              nodeId={cluster.value}
-              key={cluster.value}
-            >
-              {cluster?.children && renderCluster(cluster.children)}
-            </TreeItem>
-          ));
-        })(clusters)}
+        {this.renderCluster(clusters)}
       </TreeView>
     );
   }
