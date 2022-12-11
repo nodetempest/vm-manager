30c4070f3b510d19f5e805108bb6185a2c7edf31
Nykolai Topchyi <nodetempest@gmail.com>
2022-12-11 23:03:22 +0200

    connect redux to VMList

--- src/pages/VMList.tsx
+++ src/pages/VMList.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate, NavigateFunction } from "react-router-dom";
 import { Box, Button } from "@mui/material";
 
 import { Footer } from "~/layouts/VMWizard/Footer";
+import { AppDispatch } from "~/state/store";
+import { TVMs } from "~/state/slices/VMs/slice";
+import { RootStateType } from "~/state/rootReducer";
 
-export class VMList extends React.Component {
+export class Base extends React.Component<TWithStateProps> {
   render() {
+    const { navigate } = this.props;
     return (
       <>
         <Box
@@ -20,9 +26,37 @@ export class VMList extends React.Component {
           VMList
         </Box>
         <Footer>
-          <Button variant="contained">Close</Button>
+          <Button onClick={() => navigate("/")} variant="contained">
+            Close
+          </Button>
         </Footer>
       </>
     );
   }
 }
+
+export type TWithStateProps = {
+  navigate: NavigateFunction;
+  dispatch: AppDispatch;
+  VMs: TVMs;
+};
+
+const withState = <P extends TWithStateProps>(
+  Component: React.ComponentType<P>
+): React.FC<Omit<P, keyof TWithStateProps>> => {
+  return (props) => {
+    const navigate = useNavigate();
+    const dispatch: AppDispatch = useDispatch();
+    const VMs = useSelector<RootStateType>((state) => state.VMs);
+    return (
+      <Component
+        {...(props as P)}
+        navigate={navigate}
+        dispatch={dispatch}
+        VMs={VMs}
+      />
+    );
+  };
+};
+
+export const VMList = withState(Base);
